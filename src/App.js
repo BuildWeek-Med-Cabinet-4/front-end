4142e275736af04df13359a2a830723e44e188cd
Dominique Garrett <dmnqgrrtt@gmail.com>
2020-06-20 01:29:28 -0400

    removed test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import SignUpForm from './components/SignUpForm';
 import LoginForm from "./components/LoginForm";
